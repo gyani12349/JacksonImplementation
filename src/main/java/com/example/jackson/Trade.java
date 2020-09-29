b0597f2ae30485772bd0122941d284537fbfd6cd
nabhanabdulla <nabhu123@gmail.com>
2020-09-29 16:54:25 +0530

    Initial commit

---
+++ src/main/java/com/example/jackson/Trade.java
@@ -0,0 +1,23 @@
+
+package com.example.jackson;
+
+public class Trade {
+  
+  public String symbol;
+  public int quantity;
+  public String purchaseDate;
+  
+  public Trade() {
+  }
+  
+  public Trade(String symbol, int quantity, String purchaseDate) {
+    this.symbol = symbol;
+    this.quantity = quantity;
+    this.purchaseDate = purchaseDate;
+  }
+
+  @Override
+  public String toString() {
+    return "Trade [purchaseDate=" + purchaseDate + ", quantity=" + quantity + ", symbol=" + symbol + "]";
+  }
+}
